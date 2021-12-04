64b594f35d91e1a9d7205976c5f6e1d5c6fb6512
Manuel <wtfjoke@livenet.ch>
2021-12-04 03:26:49 +0100

    :christmas_tree: :sparkles: Add Day 3 (Part 2)

--- src/main/kotlin/com/github/wtfjoke/day3/Day3Part2.kts
+++ src/main/kotlin/com/github/wtfjoke/day3/Day3Part2.kts
@@ -1,92 +1,42 @@
 import java.nio.file.Paths
 
-val inputs = Paths.get("sample.txt").toFile()
+val inputs = Paths.get("inputs.txt").toFile()
     .useLines { it.toList() }
-
 val bitLength = inputs.first().length
 
-fun something(): List<String> {
+fun determineOxygen(): Int {
     var oxygens = inputs
     for (i in 0 until bitLength) {
+        val oxygensCount = oxygens
+            .groupingBy { it[i] }
+            .eachCount()
+        val maxEntries = oxygensCount.filterValues { it == oxygensCount.values.maxOrNull() }
+        val bitCriteria = if (maxEntries.size > 1) '1' else maxEntries.keys.first()
 
         oxygens = oxygens.filter { bit ->
-            bit[i] == oxygens
-                .groupingBy { it[i] }
-                .eachCount()
-//            .toSortedMap().lastKey()
-                .maxByOrNull { it.value }?.key
+            bit[i] == bitCriteria
         }
     }
-    return oxygens
+    return oxygens.first().toInt(2)
 }
 
-println(something().first())
-fun bla(){
-    val something = listOf("10110", "10111")
-    val eachCount = something.groupingBy { it[4] }.eachCount()
-    val sorted = eachCount.toSortedMap().lastKey()
-    println(sorted)
+fun determineCo2(): Int {
+    var co2 = inputs
+    for (i in 0 until bitLength) {
+        val co2Count = co2
+            .groupingBy { it[i] }
+            .eachCount()
+        val minEntries = co2Count.filterValues { it == co2Count.values.minOrNull() }
+        val bitCriteria = if (minEntries.size > 1) '0' else minEntries.keys.first()
+
+        co2 = co2.filter { bit ->
+            bit[i] == bitCriteria
+        }
+    }
+    return co2.first().toInt(2)
 }
 
-bla()
-//println(eachCount.toSortedMap().maxByOrNull { it.value })
-
-//var co2 = inputs
-//for (i in 0 until bitLength) {
-//    co2 = co2.filter { bit ->
-//        bit[i] == co2
-//            .groupingBy { it[i] }
-//            .eachCount()
-//            .minByOrNull { it.value }?.key
-//    }
-//}
-
-
-
-//println(co2.first())
-
-//val count = inputs
-//    .groupingBy { it.first() }
-//    .eachCount()
-//val oxygen = count
-//    .maxByOrNull { it.value }
-//
-//val co2Scrubber = count.minByOrNull { it.value }
-//
-//val oxygenNo = inputs.filter { it.first() == oxygen?.key }
-
-
-
-
-
-//val secondOxyGenCombined = oxygenNo.filter { bit ->
-//    bit[1] == oxygenNo
-//        .groupingBy { it[1] }
-//        .eachCount()
-//        .maxByOrNull { it.value }?.key
-//}
-//
-//val thirdOxyGenCombined = secondOxyGenCombined.filter { bit ->
-//    bit[2] == secondOxyGenCombined
-//        .groupingBy { it[2] }
-//        .eachCount()
-//        .maxByOrNull { it.value }?.key
-//}
-//
-//val fourthOxyGenCombined = thirdOxyGenCombined.filter { bit ->
-//    bit[3] == thirdOxyGenCombined
-//        .groupingBy { it[3] }
-//        .eachCount()
-//        .maxByOrNull { it.value }?.key
-//}
-//
-//val fifth = fourthOxyGenCombined.filter { bit ->
-//    bit[4] == fourthOxyGenCombined
-//        .groupingBy { it[4] }
-//        .eachCount()
-//        .maxByOrNull { it.value }?.key
-//}
-
+determineOxygen() * determineCo2()
 
 
 
